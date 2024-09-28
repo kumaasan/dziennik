132f9a0dbbc02709c0517f8c04bba738d312a057
kumaasan <mariuszgl06@gmail.com>
2024-09-28 14:04:35 +0200

    refactored route file

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,23 @@
 <?php
 
 use App\Http\Controllers\SubjectController;
+use App\Http\Controllers\GradeController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [SubjectController::class, 'homeController'] )->name('homePage');
 
-Route::get('/przedmioty', [\App\Http\Controllers\SubjectController::class, 'showAllSubjects'])->name('subject');
+Route::get('/przedmioty', [SubjectController::class, 'showAllSubjects'])->name('subject');
 
-Route::post('/przedmioty/oceny/srednia', [\App\Http\Controllers\GradeController::class, 'addGrade'])->name('addGrade');
+Route::post('/przedmioty/oceny/srednia', [GradeController::class, 'addGrade'])->name('addGrade');
 
-Route::get('/przedmioty/wszystkiePrzedmioty', [\App\Http\Controllers\SubjectController::class, 'showSubjectPage'])->name('subject.showAll')->middleware('auth');
+Route::get('/przedmioty/wszystkiePrzedmioty', [SubjectController::class, 'showSubjectPage'])->name('subject.showAll')->middleware('auth');
 
-Route::get('/logowanie', function (){ return view('loginPage');})->name('loginPage');
+Route::view('logowanie', 'loginPage')->name('loginPage');
 
-Route::post('/logowanie', [\App\Http\Controllers\UserController::class, 'login'])->name('login');
+Route::post('/logowanie', [UserController::class, 'login'])->name('login');
 
-Route::get('/konto/zmienHaslo',[UserController::class, 'resetPasswordForm'])->name('password.reset.form');
+Route::get('/konto/zmienHaslo',[UserController::class, 'resetPasswordForm'])->name('password.reset.form')->middleware('auth');
 
 Route::get('/logowanie/tworzenieKonta', [UserController::class, 'createAccountForm'])->name('create.account.form')->middleware('guest');
 
@@ -40,8 +41,8 @@ Route::view('/edytujOceny', 'editGrade')->name('edit.grade')->middleware('auth')
 
 Route::get('/edytujOceny', [SubjectController::class, 'editGrades'])->name('edit.grade')->middleware('auth');
 
-Route::delete('edytujOceny', [\App\Http\Controllers\GradeController::class, 'deleteGrade'])->name('delete.grade')->middleware('auth');
+Route::delete('edytujOceny', [GradeController::class, 'deleteGrade'])->name('delete.grade')->middleware('auth');
 
 Route::get('/kontakt', [UserController::class, 'showContactPage'])->name('contact.page');
 
-Route::view('/politykaPrywatnosci', 'privactPolicy')->name('policy.page');
+Route::view('/politykaPrywatnosci', 'privacyPolicy')->name('policy.page');
